ae08fb6d7a746634875b4cd1bfeae5d333f3aa27
Jatin Bansal <home.minj@gmail.com>
2024-01-30 21:40:50 +0545

    add gallery

--- front page/index.js
+++ front page/index.js
@@ -1,6 +1,11 @@
 // Your existing JavaScript code remains unchanged
 
 // Additional functions for opening and closing the login popup
+function schedule() {
+    // Specify the path to the local HTML file
+    var localWebsitePath = "/booking/booking.html"; // Replace with the actual path
+    window.open(localWebsitePath, '_blank');
+}
 function openLogin() {
     document.getElementById('loginOverlay').style.display = 'flex';
 }
@@ -8,3 +13,13 @@ function openLogin() {
 function closeLogin() {
     document.getElementById('loginOverlay').style.display = 'none';
 }
+
+document.addEventListener('contextmenu', function (e) {
+    e.preventDefault();
+});
+
+document.addEventListener('mousedown', function (e) {
+    if (e.button === 0) {
+        e.preventDefault(); // Disable left-click
+    }
+});
