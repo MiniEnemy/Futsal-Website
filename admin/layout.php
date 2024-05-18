eac0b8f3461c2aa05e30df82e4f2129862059e4b
Jatin Bansal <home.minj@gmail.com>
2024-05-18 21:10:31 +0545

    fix some code

--- admin/layout.php
+++ admin/layout.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8"> 
     <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-    <title>GeeksForGeeks</title> 
+    <title></title> 
     <script>
         let menuicn = document.querySelector(".menuicn"); 
         let nav = document.querySelector(".navcontainer"); 
