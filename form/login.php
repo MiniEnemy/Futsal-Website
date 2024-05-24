2b8ebd1205d54fb1c816478eec3663ac6dfd7b36
Frzz23 <you@example.com>
2024-05-24 20:22:45 +0545

    img

--- form/login.php
+++ form/login.php
@@ -7,7 +7,7 @@
   <title>Login Page</title>
   <link rel="stylesheet" href="../css/login2.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
 
   <script>
